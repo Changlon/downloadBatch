a33e0b3668a188f32e9d1baba87fd8505f35e15d
Changlon <changlong.a2@gmail.com>
2022-03-27 03:15:10 +0800

    页面模型ok

--- src/components/ImageDownload.vue
+++ src/components/ImageDownload.vue
@@ -4,6 +4,8 @@
    @click-left="onClickLeft"
    />
 
+
+
 <div class="post-box ">
     <div class="post-type margin-top" >
         <van-checkbox v-model="checked" class="post-type-position" name="radio"  icon-size="24px"></van-checkbox>
@@ -60,18 +62,16 @@
             src="https://cdn.jsdelivr.net/npm/@vant/assets/cat.jpeg"
         />
     </div>
-
-
-   
-
-  
     
 </div>
 
-<div  class="footer">
-   <van-button plain hairline type="primary">全选</van-button>
-    <van-button class="margin-left" plain hairline type="primary">下载</van-button>
+
+<div class="footer">
+   <van-button plain hairline type="primary" @click="checkAll">{{checkAllText}}</van-button>
+  <van-button  style="width:70%;margin-left:5px;" plain hairline type="primary" @click="download">下载</van-button>
 </div> 
+
+
 
 
 </template>
@@ -89,8 +89,20 @@ export default {
             checked
         }
     },
+    data() {
+        return {
+            checkAllText:"全选"
+        }
+    },
     methods:{
-        
+        checkAll() { 
+            this.checked = !this.checked 
+            if(this.checked) this.checkAllText = "取消" 
+            else this.checkAllText = "全选"
+        },
+        download() {
+            alert("下载成功！")
+        }
     }
 
 }
@@ -102,5 +114,5 @@ export default {
 .post-type {position: relative;margin-left: 10px;}
 .post-type-position{position:absolute;bottom: 18px;left: 8px;z-index: 1;color: #fff;}
 .ins-user-subscriber-action{position:absolute;top: 5px;right: 1px;}
-.footer{position: fixed; bottom: 40px;padding: 20px;}
+.footer{ width: 100%;position: fixed; bottom: 40px;text-align: center;}
 </style>
